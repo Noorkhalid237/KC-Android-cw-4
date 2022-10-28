1ccf26e4b0ae6d4df6a4b9db06e836fc42eef9df
Noorkhalid237 <109866546+Noorkhalid237@users.noreply.github.com>
2022-10-28 12:27:31 +0300

    Recreated classwork 4

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+              EditText firstnumber = findViewById(R.id.firstnumber);
+              EditText secondnumber = findViewById(R.id.secondnumber);
+              Button calculate = findViewById(R.id.calculate);
+              TextView total = findViewById(R.id.total);
+
+
+              calculate.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                      String one = firstnumber.getText().toString();
+                      String two = secondnumber.getText().toString();
+
+                      int x = Integer.parseInt(one);
+                      int y = Integer.parseInt(two);
+
+                      int z = x + y;
+
+                      total.setText(z + "");
+                  }
+              });
+
     }
 }
